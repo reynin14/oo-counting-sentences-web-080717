72efefb994bf79ed970ddc55ec6b2e1fc364db21
github username <github email address>
2017-08-09 16:39:01 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split("").last == "." ? true : false
   end
 
   def question?
-
+    self.split("").last == "?" ? true : false
   end
 
   def exclamation?
-
+   self.split("").last == "!" ? true : false
   end
 
   def count_sentences
+    array = self.split(/[?.!]/)
+    array.delete_if do |string|
+      string == ""
+    end
 
+    array.size
   end
+
 end
